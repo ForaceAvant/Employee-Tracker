35202c0b18fc5eda9a5595a629b35b6c9e302029
Forace Avant <themattavant@gmail.com>
2020-07-22 04:18:02 -0700

    Created connection queries for finding all Employees,Roles, and Departments in db/index.js

--- db/index.js
+++ db/index.js
@@ -17,15 +17,23 @@ class db {
     }
 
     findAllEmployees(){
-
+        return this.connection.query(
+            "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.department_name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+        );
     }
 
     findAllRoles(){
-
+        return this.connection.query(
+            "SELECT role.id, role.title, department.department_name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
+          );
+      
     }
 
     findAllDepartments(){
-
+        return this.connection.query(
+            "SELECT department.id, department.department_name FROM department"
+          );
+      
     }
 
     addEmployee(){
@@ -41,7 +49,7 @@ class db {
     }
 
     updateEmployeeRole(){
-        
+
     }
 }
 
