9670a587f0666acb1aeee5e1e79075d49958906a
Forace Avant <themattavant@gmail.com>
2020-07-22 03:22:29 -0700

    Created skeleton of index.js for the different functions that will be needed

---
+++ index.js
@@ -0,0 +1,44 @@
+const prompt = require("inquirer");
+const db = require("./db");
+require("console.table");
+
+
+//Start initial inquirer prompts
+function start() {
+
+};
+
+//Adds a department to the database
+function addDepartment() {
+
+};
+
+//Adds a role to the database
+function addRole() {
+
+};
+
+//Adds an employee to the database
+function addEmployee() {
+
+};
+
+//Views departments in database
+function viewDepartments() {
+
+};
+
+//Views roles in database
+function viewRoles() {
+
+};
+
+//Views Employees in database
+function viewEmployees() {
+
+};
+
+//Updates employee roles
+function updateEmployeeRole(){
+
+};
