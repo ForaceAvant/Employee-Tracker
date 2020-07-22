e95fa2d066ed56436056ee330e83495869ab88be
Forace Avant <themattavant@gmail.com>
2020-07-22 02:38:43 -0700

    Created tables for database in MySQL and copied creation to schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+
+USE employees;
+
+CREATE TABLE department(
+id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+department_name VARCHAR(30),
+PRIMARY KEY(id)
+);
+
+CREATE TABLE role(
+id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL NOT NULL,
+department_id INT UNSIGNED NOT NULL, 
+PRIMARY KEY(id),
+FOREIGN KEY(department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee(
+id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT UNSIGNED NOT NULL,
+manager_id INT UNSIGNED NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY(role_id) REFERENCES role(id),
+FOREIGN KEY(manager_id) REFERENCES employee(id)
+);
